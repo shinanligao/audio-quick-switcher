3668bde3113def051f190339e637b5d0a694db0a
Shi Nan Li Gao <shinan.ligao@proton.me>
2025-06-06 15:32:11 +0200

    prettify

--- extension.js
+++ extension.js
@@ -15,12 +15,10 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
 export default class PlainExampleExtension extends Extension {
-    enable() {
-    }
+    enable() {}
 
-    disable() {
-    }
+    disable() {}
 }
